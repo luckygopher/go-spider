a179a143bd4b7e76796265b70a8c6d4cd397fa43
qingyunjun <mr.arvin.hou@gmail.com>
2020-10-13 23:26:01 +0800

    编写单任务引擎

--- engine/current.go
+++ engine/current.go
@@ -12,5 +12,26 @@ func NewCurrentEngine() *CurrentEngine {
 
 // 引擎启动方法
 func (c *CurrentEngine) Run(seeds ...Request) {
+	// 声明一个请求种子队列
+	var requests []Request
+	// 遍历push任务到队列中
+	for _, seed := range seeds {
+		requests = append(requests, seed)
+	}
+	// for循环pop队列，直到队列为空
+	for len(requests) > 0 {
+		// 取出一个种子
+		//seed := requests[0]
+		// 重新赋值种子队列
+		//requests = requests[1:]
+		// TODO 调用任务处理
+		// TODO 处理任务返回结果,将探索到的 seed 添加到队列中继续处理
+	}
+}
 
+// 任务处理
+func worker(r Request) (ParserResult, error) {
+	// TODO 1、需要实现fetch爬取到种子的html内容
+	// TODO 2、需要实现parser解析fetch的html内容
+	// TODO 3、返回我们解析结果给引擎做后续处理
 }
