46a7bf206d77b5ac7b5105349c1842b99a326507
qingyunjun <mr.arvin.hou@gmail.com>
2020-10-28 00:28:59 +0800

    使用xpath获取html元素

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go-spider
 go 1.14
 
 require (
+	github.com/antchfx/htmlquery v1.2.3
 	golang.org/x/net v0.0.0-20201010224723-4f7140c49acb
 	golang.org/x/text v0.3.3
 )
--- parser/pilishen/article_list.go
+++ parser/pilishen/article_list.go
@@ -2,10 +2,26 @@ package pilishen
 
 import (
 	"fmt"
+	"github.com/antchfx/htmlquery"
 	"go-spider/engine"
+	"strings"
 )
 
 func Parser(body []byte) engine.ParserResult {
-	fmt.Printf("body:%s",body)
+	// 使用 xpath 获取数据
+	doc, err := htmlquery.Parse(strings.NewReader(string(body)))
+	if err != nil {
+		fmt.Errorf("xpath parse err: %v", err)
+	}
+	// 使用 xpath 匹配数据
+	articleListData := htmlquery.Find(doc, `//*[@id="app"]/div/div/div[1]/div`)
+	// 遍历匹配的html节点
+	for _, item := range articleListData {
+		// 使用 xpath 获取到 item 的html
+		itemHtml := htmlquery.OutputHTML(item, true)
+		fmt.Println(itemHtml)
+		// TODO: 对获取到 item 进行正则或者 xpath 匹配，获取需要的数据
+	}
+	// 返回解析结果
 	return engine.ParserResult{}
 }
